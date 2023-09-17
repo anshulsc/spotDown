afd0eded3fe708cd3add82329480d915697d9a69
anshulsc <anshulsinghchambial@gmail.com>
2023-09-17 16:51:00 +0530

    Uploaded

--- stremlit.py
+++ stremlit.py
@@ -12,6 +12,10 @@ load_dotenv()
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
+st.set_page_config(layout="wide")
+
+
+
 
 from spotDown.spotify import get_token, get_playlist_tracks, query_one
 
@@ -97,6 +101,7 @@ st.markdown("""
 <style>
 .song-details-container {
     display: flex;
+    flex-wrap: wrap;
     justify-content: space-between;
 }
 
@@ -108,6 +113,7 @@ st.markdown("""
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
     transition: transform 0.3s ease;
 }
+
 .song-card:hover {
     background-color: #f5f5f3;
     transform: scale(1.05);
@@ -116,7 +122,18 @@ st.markdown("""
     border-radius: 10px;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 }
-            
+
+.image-card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    padding: 20px;
+    border-radius: 10px;
+    transition: transform 0.3s ease;
+}
+
+
 .image-card img {
     max-width: 100%;
     border-radius: 10px;
@@ -126,43 +143,43 @@ st.markdown("""
 .image-card:hover img {
     transform: scale(1.1);
 }
- .video-card {
-            background-color: #f5f5f5;
-            padding: 20px;
-            margin: 10px;
-            border-radius: 10px;
-            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-            text-align: center;
-        }
+
+.video-card {
+    background-color: #f5f5f5;
+    padding: 20px;
+    margin: 10px;
+    border-radius: 10px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    text-align: center;
+}
 
 video {
-            width: 100%;
-            border-radius: 20px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-            outline: none;
-        }
-
-
+    width: 100%;
+    border-radius: 20px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+    outline: none;
+}
 
 video::after {
-            content: "\\25B6";
-            font-size: 3rem;
-            color: #fff;
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            cursor: pointer;
-        }
+    content: "\\25B6";
+    font-size: 3rem;
+    color: #fff;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    cursor: pointer;
+}
 
-        /* Hover effect for the play button */
+/* Hover effect for the play button */
 video:hover::after {
-            transform: translate(-50%, -50%) scale(1.2);
-        }
-            
+    transform: translate(-50%, -50%) scale(1.2);
+}
+
 h2 {
     font-size: 24px;
     margin: 0;
+    color: #000;
 }
 
 em {
@@ -171,6 +188,37 @@ em {
 
 p {
     margin: 5px 0;
+    color: #000;
+}
+
+/* Dark mode styles */
+.dark-mode .song-card,
+.dark-mode .image-card,
+.dark-mode .video-card {
+    background-color: #333;
+    box-shadow: 0 4px 6px rgba(255, 255, 255, 0.1);
+    color: #fff;
+}
+
+/* Media queries for responsiveness */
+@media (max-width: 768px) {
+    .song-details-container {
+        flex-direction: column;
+    }
+
+    .song-card,
+    .image-card,
+    .video-card {
+        width: 100%;
+    }
+
+    .image-card {
+        text-align: center;
+    }
+
+    video::after {
+        font-size: 2rem;
+    }
 }
 
 </style>
